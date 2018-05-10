5646ec50ebe57aa8a5ee390799ee967ee88c1f5c
Deivison-PC\Deivison <dguarines@gmail.com>
2018-05-10 18:51:07 -0300

    Apps basic development

--- App.js
+++ App.js
@@ -1,58 +1,113 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Image
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class mimimiGenerator extends Component{
+  constructor(props){
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+    this.state = {text1:'Text1',
+                  text2:'Text2'};
+
+    this.typedText = this.typedText.bind(this);
+  }
+
+  changeVowel(t){
+    let newText = t.toLowerCase();
+
+    newText = newText.replace(/(a|e|i|o|u)/g, 'i');
+		newText = newText.replace(/(á|à|ã|â)/g, 'i');
+		newText = newText.replace(/(é|è|ê)/g, 'i');
+		newText = newText.replace(/(í|ì|î)/g, 'i');
+		newText = newText.replace(/(ó|ò|ô)/g, 'i');
+		newText = newText.replace(/(ú|ù|û)/g, 'i');
+
+    return newText;
+  }
+
+  typedText(t){
+      let localState = this.state;
+      localState.text1 = t;
+      localState.text2 = this.changeVowel(t);
+      this.setState(localState);
+  }
+
+  render(){
+    return(
+        <View style={styles.body}>
+            <View>
+                <Text style={styles.title}> Mimimi Generator </Text>
+            </View>
+
+            <View style={styles.inputArea}>
+                <TextInput style={styles.input} placeholder="Type your mimimi" onChangeText={this.typedText}/>
+            </View>
+
+            <View style={styles.area}>
+                <Text style={[styles.text, styles.text1]}> {this.state.text1.toUpperCase()}</Text>
+                <Image style={styles.boy} source={require('./images/mimimi.jpg')} />
+                <Text style={[styles.text, styles.text2]}> {this.state.text2.toUpperCase()} </Text>
+            </View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    body:{
+      backgroundColor:'#999999',
+      paddingTop:30,
+      flex:1,
+      flexDirection:'column',
+      alignItems:'center'
+    },
+    title:{
+      fontSize:30,
+      color:'#FFFFFF'
+    },
+    inputArea:{
+      alignSelf:'stretch'
+    },
+    input:{
+      borderWidth:1,
+      borderColor:'#999999',
+      backgroundColor:'#EEEEEE',
+      color:'#000000',
+      height:40,
+      margin:20,
+      padding:10
+    },
+    area:{
+      width:300,
+      height:300,
+      marginTop:10
+    },
+    boy:{
+      width:300,
+      height:300,
+      marginTop:-70,
+      zIndex:0
+
+    },
+    text:{
+      fontSize:25,
+      color:'white',
+      padding:10,
+      backgroundColor:'transparent',
+      fontWeight:'bold',
+      textAlign:'center',
+      height:50
+    },
+    text1:{
+      zIndex:1
+    },
+    text2:{
+      zIndex:1,
+      marginTop:-70
+    }
 });
